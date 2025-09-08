82c2a7ec33819e76e174b65523b53a1f0dd58356
Ujjaval Saini <sainiujvl@gmail.com>
2025-09-08 23:08:09 +0530

    added backend of icons

--- app/src/main/java/com/uv/androidassessment/ui/MainActivity.kt
+++ app/src/main/java/com/uv/androidassessment/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.uv.androidassessment.ui
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -21,16 +23,32 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.userData.observe(this) { user ->
             if (user != null) {
+                // Basic Info
                 binding.tvName.text = user.name
                 binding.tvUsername.text = user.username
                 binding.tvLocation.text = "${user.location.city}, ${user.location.country}"
-                binding.tvFollowers.text = "Followers: ${user.statistics.followers}"
-                binding.tvFollowing.text = "Following: ${user.statistics.following}"
 
-                // Load image using Glide
+                // Statistics
+                binding.tvFollowers.text = "${user.statistics.followers}"
+                binding.tvFollowing.text = "${user.statistics.following}"
+//                binding.tvShots.text = "Shots: ${user.statistics.activity.shots}"
+//                binding.tvCollections.text = "Collections: ${user.statistics.activity.collections}"
+//
+//                // Socials
+//                binding.tvWebsite.text = user.social.website
+//                binding.tvWebsite.movementMethod = LinkMovementMethod.getInstance() // make link clickable
+
+                // Profiles (Instagram, Facebook, etc.)
+                val profilesText = user.social.profiles.joinToString("\n") {
+                    "${it.platform}: ${it.url}"
+                }
+//                binding.tvProfiles.text = profilesText
+
+                // Avatar
                 Glide.with(this)
                     .load(user.avatar)
                     .into(binding.ivAvatar)
+
             } else {
                 binding.tvName.text = "Error loading user"
             }
